ce125d545e1c75a474eba10af859710b26e98e1d
Gangadhara Varma <gangadharap@nit.ad>
2021-05-14 11:00:55 +0530

    cosmitcs branch

---
+++ client/build/precache-manifest.ec3536b2cee64a0ddfa5a2b0043b1da6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389197b75667c97e5a3de9bafc8a6f84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a14eca9b1363403b1acd",
+    "url": "/static/css/2.b6ac9e22.chunk.css"
+  },
+  {
+    "revision": "2c7085a5af8d5bb9cdbc",
+    "url": "/static/css/main.ea1027fc.chunk.css"
+  },
+  {
+    "revision": "a14eca9b1363403b1acd",
+    "url": "/static/js/2.3e9664a7.chunk.js"
+  },
+  {
+    "revision": "2c7085a5af8d5bb9cdbc",
+    "url": "/static/js/main.f8b79d66.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
